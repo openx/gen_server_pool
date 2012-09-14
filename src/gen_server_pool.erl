4594c3f03cef1873fad3c76d71076087ad39e4f6
Anthony Molinaro <anthony.molinaro@openx.com>
2012-09-14 16:02:05 -0700

    sync with openx

--- src/gen_server_pool.erl
+++ src/gen_server_pool.erl
@@ -1,33 +1,7 @@
-%%  Copyright (c) 2011, OpenX
-%%  All rights reserved.
-%%  
-%%  Redistribution and use in source and binary forms, with or without
-%%  modification, are permitted provided that the following conditions are met:
-%%      * Redistributions of source code must retain the above copyright
-%%        notice, this list of conditions and the following disclaimer.
-%%      * Redistributions in binary form must reproduce the above copyright
-%%        notice, this list of conditions and the following disclaimer in the
-%%        documentation and/or other materials provided with the distribution.
-%%      * Neither the name of the <organization> nor the
-%%        names of its contributors may be used to endorse or promote products
-%%        derived from this software without specific prior written permission.
-%%  
-%%  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-%%  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-%%  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-%%  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-%%  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-%%  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-%%  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-%%  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-%%  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-%%  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 %%%-------------------------------------------------------------------
 %%% File    : gen_server_pool.erl
 %%% Author  : Joel Meyer <joel@openx.org>
-%%% Description : 
+%%% Description :
 %%%   A pool for gen_servers.
 %%%
 %%% Created :  4 May 2011
@@ -40,7 +14,10 @@
 -export([ start_link/4,
           start_link/5,
           get_stats/1,
-          available/2 ]).
+          get_pool_pids/1,
+          available/3,
+          unavailable/2
+        ]).
 
 %% gen_server callbacks
 -export([ init/1,
@@ -50,7 +27,8 @@
           terminate/2,
           code_change/3 ]).
 
--record(state, { sup_pid,
+-record(state, { proxy_ref,
+                 sup_pid,
                  sup_max_r,
                  sup_max_t = 1,
                  available = [],
@@ -58,8 +36,16 @@
                  min_size  = 0,
                  max_size  = 10,
                  idle_secs = infinity,
+                 max_queued_tasks = infinity,
+                 num_queued_tasks = 0,
+                 num_dropped_tasks = 0,
                  module,
-                 pool_id }).
+                 pool_id,
+                 prog_id,
+                 wm_size = 0,
+                 wm_active = 0,
+                 wm_tasks = 0
+                 }).
 
 %%====================================================================
 %% API
@@ -77,14 +63,16 @@ start_link( Name, Module, Args, Options, PoolOpts ) ->
                          Options ).
 
 get_stats( MgrPid ) ->
-  gen_server:call( MgrPid, { gen_server_pool, stats } ).
+  gen_server:call( MgrPid, gen_server_pool_stats ).
 
+available( MgrPid, ProxyRef, WorkerPid ) ->
+  gen_server:cast( MgrPid, { ProxyRef, worker_available, { WorkerPid, now() } } ).
 
-available( MgrPid, WorkerPid ) ->
-  gen_server:cast( MgrPid, { gen_server_pool,
-                             worker_available,
-                             { WorkerPid, now() } } ).
+unavailable( MgrPid, WorkerPid ) ->
+  gen_server:call( MgrPid, { unavailable, WorkerPid } ).
 
+get_pool_pids( MgrPid ) ->
+  gen_server:call( MgrPid, get_pool_pids ).
 
 %%====================================================================
 %% gen_server callbacks
@@ -97,15 +85,15 @@ available( MgrPid, WorkerPid ) ->
 %%                         {stop, Reason}
 %% Description: Initiates the server
 %%--------------------------------------------------------------------
-init( [ Module, Args, Options, PoolOpts ]  ) ->
+init( [ Module, Args, Options, PoolOpts ] ) ->
   % Init state from opts
   S = parse_opts( PoolOpts, #state{ module = Module } ),
 
   % Get config from state
-  #state{ sup_max_r = MaxR, sup_max_t = MaxT } = S,
+  #state{ proxy_ref = ProxyRef, sup_max_r = MaxR, sup_max_t = MaxT } = S,
 
   % Start supervisor for pool members
-  { ok, SupPid } = gen_server_pool_sup:start_link( self(),
+  { ok, SupPid } = gen_server_pool_sup:start_link( self(), ProxyRef,
                                                    Module, Args, Options,
                                                    MaxR, MaxT ),
 
@@ -114,6 +102,8 @@ init( [ Module, Args, Options, PoolOpts ]  ) ->
   case assure_min_pool_size( State ) of
     ok ->
       schedule_idle_check( State ),
+      schedule_collect_stats( State ),
+      schedule_emit_stats( State ),
       { ok, State };
     Error ->
       { stop, Error }
@@ -132,21 +122,43 @@ init( [ Module, Args, Options, PoolOpts ]  ) ->
 handle_call( gen_server_pool_stats, _From, State ) ->
   { reply, stats( State ), State };
 
-handle_call( Call, From, State ) ->
-  NState = handle_request( { '$gen_call', From, Call }, State ),
-  { noreply, NState }.
+handle_call( get_pool_pids, _From, State = #state{ sup_pid = SupPid } ) ->
+  Children = supervisor:which_children(SupPid),
+  { reply, {ok, Children}, State };
 
+handle_call( {unavailable, Pid}, _From, State ) ->
+  NState = worker_unavailable( Pid, State ),
+  { reply, {ok, Pid}, NState };
+
+handle_call( Call, From, State=#state{ max_queued_tasks=infinity }) ->
+  { noreply, handle_request( { '$gen_call', From, Call }, State ) };
+handle_call( _Call, _From, State=#state{ num_queued_tasks=NumTasks,
+                                       num_dropped_tasks=DroppedTasks,
+                                       max_queued_tasks=MaxTasks,
+                                       module = _Module}) 
+                                       when NumTasks > MaxTasks ->
+  { reply, {error, request_dropped}, State#state{ num_dropped_tasks = DroppedTasks + 1} };
+handle_call( Call, From, State ) ->
+  { noreply, handle_request( { '$gen_call', From, Call }, State ) }.
 %%--------------------------------------------------------------------
 %% Function: handle_cast(Msg, State) -> {noreply, State} |
 %%                                      {noreply, State, Timeout} |
 %%                                      {stop, Reason, State}
 %% Description: Handling cast messages
 %%--------------------------------------------------------------------
-handle_cast( { gen_server_pool, worker_available, PidTime }, State ) ->
+handle_cast( { ProxyRef, worker_available, PidTime },
+             State = #state{ proxy_ref = ProxyRef } ) ->
   { noreply, worker_available( PidTime, State ) };
 
+handle_cast( Cast, State=#state{ max_queued_tasks=infinity }) ->
+  { noreply, handle_request( { '$gen_cast', Cast }, State ) };
+handle_cast( _Cast, State=#state{ num_queued_tasks=NumTasks,
+                                 num_dropped_tasks=DroppedTasks,
+                                 max_queued_tasks=MaxTasks }) 
+                               when NumTasks > MaxTasks ->
+  { noreply, State#state{ num_dropped_tasks = DroppedTasks + 1} };
 handle_cast( Cast, State ) ->
-  { noreply, handle_request( { '$gen_cast', Cast }, State ) }.
+  { noreply, handle_request( {'$gen_cast', Cast }, State ) }.
 
 %%--------------------------------------------------------------------
 %% Function: handle_info(Info, State) -> {noreply, State} |
@@ -154,10 +166,30 @@ handle_cast( Cast, State ) ->
 %%                                       {stop, Reason, State}
 %% Description: Handling all non call/cast messages
 %%--------------------------------------------------------------------
-handle_info( { gen_server_pool, check_idle_timeouts }, State ) ->
+handle_info( { ProxyRef, emit_stats },
+             #state{ proxy_ref = ProxyRef } = State ) ->
+  NewState = emit_stats( State ),
+  schedule_emit_stats( State ),
+  { noreply, NewState};
+
+handle_info( { ProxyRef, collect_stats },
+             #state{ proxy_ref = ProxyRef } = State ) ->
+  NewState = collect_stats( State ),
+  schedule_collect_stats( State ),
+  { noreply, NewState };
+
+handle_info( { ProxyRef, check_idle_timeouts },
+             #state{ proxy_ref = ProxyRef } = State ) ->
   schedule_idle_check( State ),
   { noreply, check_idle_timeouts( State ) }; 
 
+handle_info( Info, State=#state{ max_queued_tasks=infinity }) ->
+  { noreply, handle_request( Info, State ) };
+handle_info( _Info, State=#state{ num_queued_tasks=NumTasks,
+                                 num_dropped_tasks=DroppedTasks,
+                                 max_queued_tasks=MaxTasks }) 
+                               when NumTasks > MaxTasks ->
+  { noreply, State#state{ num_dropped_tasks = DroppedTasks + 1} };
 handle_info( Info, State ) ->
   { noreply, handle_request( Info, State ) }.
 
@@ -182,8 +214,12 @@ code_change( _OldVsn, State, _Extra ) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
-handle_request( Req, State = #state{ requests = { Push, Pop } } ) ->
-  do_work( State#state{ requests = { [ Req | Push ], Pop } } ).
+handle_request( Req, State = #state{ requests = { Push, Pop },
+                                     num_queued_tasks=NumTasks } ) ->
+  do_work( State#state{ requests = { [ Req | Push ], Pop }, 
+                        num_queued_tasks=NumTasks+1} ).
+
+%  do_work( State#state{ requests = { [ Req | Push ], Pop }} ).
 
 
 worker_available( { Pid, Time }, State = #state{ available = Workers } ) ->
@@ -194,21 +230,71 @@ worker_available( { Pid, Time }, State = #state{ available = Workers } ) ->
   end.
 
 
+worker_unavailable( Pid, State = #state{ available = Workers } ) ->
+  State#state{ available = proplists:delete( Pid, Workers ) }.
+
 stats( #state{ sup_pid   = SupPid,
                available = Workers,
-               requests  = { Push, Pop } } ) ->
+%               requests  = { Push, Pop },
+               wm_size = WmSize, 
+               wm_active = WmActive, 
+               wm_tasks = WmTasks,
+               num_queued_tasks = NumTasks,
+               num_dropped_tasks = NumDroppedTasks
+               } ) ->
   Size   = proplists:get_value( active, supervisor:count_children( SupPid ) ),
   Idle   = length( Workers ),
   Active = Size - Idle,
-  Tasks  = length( Push ) + length( Pop ),
-  [ { size, Size }, { active, Active }, { idle, Idle }, { tasks, Tasks } ].
+%  Tasks  = length( Push ) + length( Pop ),
+  WmIdle = WmSize - WmActive,
+  [ { size, Size }, { active, Active }, { idle, Idle }, { tasks, NumTasks },
+    { wmsize, WmSize}, { wmactive, WmActive}, { wmidle, WmIdle}, { wmtasks, WmTasks},
+    { drops, NumDroppedTasks}
+  ].
+
+collect_stats ( State = #state{ sup_pid = SupPid,
+%                                requests = { Push, Pop },
+                                available = Workers,
+                                wm_size = WmSize, wm_active = WmActive, 
+                                wm_tasks = WmTasks,
+                                num_queued_tasks=NumTasks } ) ->
+  Size   = proplists:get_value( active, supervisor:count_children( SupPid ) ),
+  Idle   = length( Workers ),
+  Active = Size - Idle,
+%  Tasks  = length( Push ) + length( Pop ),
+  Tasks  = NumTasks,
+
+  NewWmSize = max(Size,WmSize),
+  NewWmActive = max (Active, WmActive),
+  NewWmTasks = max(Tasks, WmTasks),
+
+  State#state{wm_size = NewWmSize, wm_active = NewWmActive, wm_tasks = NewWmTasks}.
+
+emit_stats( #state{ prog_id = ProgId, pool_id = PoolId } = S ) ->
+  N = fun( K ) -> 
+        lists:flatten( [ PoolId, [ "_" | atom_to_list( K ) ] ] )
+      end,
+
+  M = lists:foldl( fun( { K, V }, A ) ->
+                        [ { gauge, N( K ), V } | A ]
+                   end,
+                   [],
+                   stats( S ) ),
+
+  mondemand:send_stats( ProgId, [], M ),
+  % reset stats after emit
+  S#state{ 
+     wm_size = 0,
+     wm_active = 0,
+     wm_tasks = 0,
+     num_dropped_tasks=0
+  }.
 
 
-
+% reset stats after emit
 terminate_pool( _Reason, _State ) ->
   ok.
-
-
+  
 do_work( State = #state{ requests  = { [], [] } } ) ->
   % No requests - do nothing.
   State;
@@ -229,16 +315,17 @@ do_work( State = #state{ requests  = { Push, [] } } ) ->
   do_work( State#state{ requests = { [], lists:reverse( Push ) } } );
 
 do_work( State = #state{ available = [ { Pid, _ } | Workers ],
-                         requests  = { Push, [ Req | Pop ] } } ) ->
+                         requests  = { Push, [ Req | Pop ] },
+                         num_queued_tasks = NumTasks } ) ->
   case is_process_alive(Pid) of
     false ->
       do_work( State#state{ available = Workers } );
     true  ->
       erlang:send( Pid, Req, [noconnect] ),
       State#state{ available = Workers,
-                   requests  = { Push, Pop } }
+                   requests  = { Push, Pop },
+                   num_queued_tasks=NumTasks-1}
   end.
-
 
 assure_min_pool_size( #state{ min_size = MinSize, sup_pid = SupPid } = S ) ->
   PoolSize = proplists:get_value( active, supervisor:count_children( SupPid ) ),
@@ -256,33 +343,46 @@ add_children( N, #state{ sup_pid = SupPid } = S ) ->
 
 schedule_idle_check( #state{ idle_secs = infinity } = S ) ->
   S;
-schedule_idle_check( #state{ idle_secs = IdleSecs } = S ) ->
-  erlang:send_after( IdleSecs * 1000, self(), { gen_server_pool, check_idle_timeouts } ),
+schedule_idle_check( #state{ proxy_ref = ProxyRef,
+                             idle_secs = IdleSecs } = S ) ->
+  erlang:send_after( IdleSecs * 1000, self(), { ProxyRef, check_idle_timeouts } ),
   S.
 
 
 check_idle_timeouts( #state{ available = [] } = S ) ->
   S;
-check_idle_timeouts( #state{ idle_secs = IdleSecs,
-                             available = Available } = S ) ->
-  Now = now(),
-  F = fun( { Pid, Time }, Acc ) ->
-        IdleTime = seconds_between( Now, Time ),
-        case IdleTime >= IdleSecs of
-          true ->
-            gen_server_pool_proxy:stop( Pid ),
-            Acc;
-          false ->
-            [ { Pid, Time } | Acc ]
-        end
-      end,
+check_idle_timeouts( #state{ proxy_ref = ProxyRef,
+                             sup_pid = SupPid,
+                             idle_secs = IdleSecs,
+                             available = Available,
+                             min_size = MinSize } = S ) ->
+  PoolSize = proplists:get_value( active, supervisor:count_children( SupPid ) ),
+  MaxWorkersToKill = PoolSize - MinSize,
+  Survivors = kill_idle_workers (ProxyRef,IdleSecs,Available, [], MaxWorkersToKill),
 
-  State = S#state{ available = lists:foldl( F, [], Available ) },
-
+  State = S#state{ available = Survivors },
   assure_min_pool_size( State ),
 
   State.
 
+kill_idle_workers(_ProxyRef,_IdleSecs,Available, Survivors, MaxWorkersToKill)  when MaxWorkersToKill =< 0 ->
+  Available ++ Survivors;
+kill_idle_workers(_ProxyRef,_IdleSecs,[], Survivors, _MaxWorkersToKill) ->
+  Survivors;
+kill_idle_workers(ProxyRef,IdleSecs,[{Pid,Time} | Available], Survivors, MaxWorkersToKill) ->
+  Now = now(),
+  IdleTime = seconds_between( Now, Time ),
+  case IdleTime >= IdleSecs of
+    true ->
+      case is_process_alive(Pid) of
+        false -> kill_idle_workers(ProxyRef,IdleSecs, Available, Survivors, MaxWorkersToKill);
+        true ->
+          gen_server_pool_proxy:stop( Pid, ProxyRef ),
+          kill_idle_workers(ProxyRef,IdleSecs, Available, Survivors, MaxWorkersToKill-1)
+      end;
+    false ->
+      kill_idle_workers(ProxyRef,IdleSecs, Available, [ {Pid, Time} | Survivors], MaxWorkersToKill)
+  end.
 
 seconds_between( { MS1, S1, _ }, { MS1, S2, _ } ) ->
   S1 - S2;
@@ -290,18 +390,36 @@ seconds_between( { MS1, S1, _ }, { MS2, S2, _ } ) ->
   ( MS1 * 1000000 + S1 ) - ( MS2 * 1000000 + S2 ).
 
 
+schedule_emit_stats( #state{ prog_id = undefined } ) ->
+  % No prog_id, so can't emit stats
+  ok;
+schedule_emit_stats( #state{ pool_id = undefined } ) ->
+  % No pool_id, so can't emit stats
+  ok;
+schedule_emit_stats( #state{ proxy_ref = ProxyRef } ) ->
+  % pool_id and prog_id are set, can emit stats
+  erlang:send_after( 60*1000, self(), { ProxyRef, emit_stats } ).
+
+schedule_collect_stats( #state{ proxy_ref = ProxyRef } ) ->
+  % collect stats every 10 second.
+  erlang:send_after( 1*1000, self(), { ProxyRef, collect_stats } ).
+
 parse_opts( [], State ) ->
   finalize( State );
 parse_opts( [ { pool_id, V } | Opts ], State ) when is_atom( V ) ->
   parse_opts( [ { pool_id, atom_to_list( V ) } | Opts ], State );
 parse_opts( [ { pool_id, V } | Opts ], State ) ->
   parse_opts( Opts, State#state{ pool_id = V } );
+parse_opts( [ { prog_id, V } | Opts ], State ) ->
+  parse_opts( Opts, State#state{ prog_id = V } );
 parse_opts( [ { min_pool_size, V } | Opts ], State ) ->
   parse_opts( Opts, State#state{ min_size = V } );
 parse_opts( [ { max_pool_size, V } | Opts ], State ) ->
   parse_opts( Opts, State#state{ max_size = V } );
 parse_opts( [ { idle_timeout, V } | Opts ], State ) ->
   parse_opts( Opts, State#state{ idle_secs = V } );
+parse_opts( [ { max_queue, V } | Opts ], State ) ->
+  parse_opts( Opts, State#state{ max_queued_tasks = V } );
 parse_opts( [ { sup_max_r, V } | Opts ], State ) ->
   parse_opts( Opts, State#state{ sup_max_r = V } );
 parse_opts( [ { sup_max_t, V } | Opts ], State ) ->
@@ -312,7 +430,8 @@ finalize( #state{ sup_max_r = undefined, max_size = Sz } = S ) ->
   % Set max_r to max pool size if not set
   finalize( S#state{ sup_max_r = Sz } );
 finalize( State ) ->
-  State.
+  % Add unique reference for this proxy
+  State#state{ proxy_ref = make_ref() }.
 
 
 %%--------------------------------------------------------------------
--- src/gen_server_pool_proxy.erl
+++ src/gen_server_pool_proxy.erl
@@ -1,29 +1,3 @@
-%%  Copyright (c) 2011, OpenX
-%%  All rights reserved.
-%%  
-%%  Redistribution and use in source and binary forms, with or without
-%%  modification, are permitted provided that the following conditions are met:
-%%      * Redistributions of source code must retain the above copyright
-%%        notice, this list of conditions and the following disclaimer.
-%%      * Redistributions in binary form must reproduce the above copyright
-%%        notice, this list of conditions and the following disclaimer in the
-%%        documentation and/or other materials provided with the distribution.
-%%      * Neither the name of the <organization> nor the
-%%        names of its contributors may be used to endorse or promote products
-%%        derived from this software without specific prior written permission.
-%%  
-%%  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-%%  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-%%  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-%%  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-%%  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-%%  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-%%  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-%%  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-%%  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-%%  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
 %%%-------------------------------------------------------------------
 %%% File    : gen_server_pool_proxy.erl
 %%% Author  : Joel Meyer <joel@openx.org>
@@ -37,8 +11,8 @@
 -behaviour(gen_server).
 
 %% API
--export([ start_link/4,
-          stop/1 ]).
+-export([ start_link/5,
+          stop/2 ]).
 
 %% gen_server callbacks
 -export([ init/1,
@@ -49,58 +23,64 @@
           code_change/3 ]).
 
 -record( state, { manager_pid,
+                  proxy_ref,
                   module,
                   state } ).
 
 %%====================================================================
 %% API
 %%====================================================================
-start_link( MgrPid, Module, Args, Options ) ->
-  gen_server:start_link( ?MODULE, [ MgrPid, Module, Args ], Options ).
+start_link( MgrPid, ProxyRef, Module, Args, Options ) ->
+  gen_server:start_link( ?MODULE,
+                         [ MgrPid, ProxyRef, Module, Args ], Options ).
 
-stop( Pid ) ->
-  gen_server:call( Pid, { gen_server_pool_proxy, stop } ).
+stop( Pid, ProxyRef ) ->
+  gen_server:call( Pid, { ProxyRef, stop } ).
 
 %%====================================================================
 %% gen_server callbacks
 %%====================================================================
 
-init( [ MgrPid, Module, Args ] ) ->
+init( [ MgrPid, ProxyRef, Module, Args ] ) ->
   PState = #state{ manager_pid = MgrPid,
+                   proxy_ref   = ProxyRef,
                    module      = Module },
 
   case Module:init( Args ) of
     { ok, State } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { ok, state( PState, State ) };
     { ok, State, Extra } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { ok, state( PState, State ), Extra };
     Other ->
       Other
   end.
 
 
-handle_call( { gen_server_pool_proxy, stop }, _From, PState ) ->
+handle_call( { ProxyRef, stop },
+             _From,
+             #state{ proxy_ref = ProxyRef } = PState ) ->
   { stop, normal, ok, PState };
 
 handle_call( Msg,
              From,
              #state{ manager_pid = MgrPid,
+                     proxy_ref   = ProxyRef,
                      module      = M,
                      state       = S } = PState ) ->
   case M:handle_call( Msg, From, S ) of
     { reply, Reply, NewS } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { reply, Reply, state( PState, NewS ) };
     { reply, Reply, NewS, Extra } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { reply, Reply, state( PState, NewS ), Extra };
     { noreply, NewS } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ) };
     { noreply, NewS, Extra } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ), Extra };
     { stop, Reason, Reply, NewS } ->
       { stop, Reason, Reply, state( PState, NewS ) };
@@ -111,14 +91,15 @@ handle_call( Msg,
 
 handle_cast( Msg,
              #state{ manager_pid = MgrPid,
+                     proxy_ref   = ProxyRef,
                      module      = M,
                      state       = S } = PState ) ->
   case M:handle_cast( Msg, S ) of
     { noreply, NewS } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ) };
     { noreply, NewS, Extra } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ), Extra };
     { stop, Reason, NewS } ->
       { stop, Reason, state( PState, NewS ) }
@@ -127,14 +108,15 @@ handle_cast( Msg,
 
 handle_info( Msg,
              #state{ manager_pid = MgrPid,
+                     proxy_ref   = ProxyRef,
                      module      = M,
                      state       = S } = PState ) ->
   case M:handle_info( Msg, S ) of
     { noreply, NewS } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ) };
     { noreply, NewS, Extra } ->
-      gen_server_pool:available( MgrPid, self() ),
+      gen_server_pool:available( MgrPid, ProxyRef, self() ),
       { noreply, state( PState, NewS ), Extra };
     { stop, Reason, NewS } ->
       { stop, Reason, state( PState, NewS ) }
@@ -151,7 +133,7 @@ code_change( OldVsn,
              #state{ module = M, state = S } = PState,
              Extra ) ->
   { ok, NewS } = M:code_change( OldVsn, S, Extra ),
-  { ok, state( PState, NewS ) }.
+  {ok, state( PState, NewS ) }.
 
 %%--------------------------------------------------------------------
 %%% Internal functions
