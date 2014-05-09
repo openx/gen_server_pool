060ecf75d8c78a0fd3a1e4b6d60a8685afc64a0a
Yoshihiro Tanaka <hirotnkg@gmail.com>
2014-05-08 22:21:35 -0700

    Use `gen_server:cast/2` to stop worker processes
    gen_server_pool can die in the following cases:
    1. timeout
    2. the worker process has gone for some reason
    I have seen these cases happening under the high load.
    In addition since it's not checking the return value of
    `gen_server_pool_proxy:stop/2`, it looks better to use
    `gen_server:cast/2`.

--- src/gen_server_pool_proxy.erl
+++ src/gen_server_pool_proxy.erl
@@ -37,7 +37,7 @@ start_link( MgrPid, ProxyRef, Module, Args, Options ) ->
                          [ MgrPid, ProxyRef, Module, Args ], Options ).
 
 stop( Pid, ProxyRef ) ->
-  gen_server:call( Pid, { ProxyRef, stop } ).
+  gen_server:cast( Pid, { ProxyRef, stop } ).
 
 %%====================================================================
 %% gen_server callbacks
@@ -92,6 +92,10 @@ handle_call( Msg,
       { stop, Reason, state( PState, NewS ) }
   end.
 
+
+handle_cast( { ProxyRef, stop },
+             #state{ proxy_ref = ProxyRef } = PState ) ->
+  { stop, normal, PState };
 
 handle_cast( Msg,
              #state{ manager_pid = MgrPid,
